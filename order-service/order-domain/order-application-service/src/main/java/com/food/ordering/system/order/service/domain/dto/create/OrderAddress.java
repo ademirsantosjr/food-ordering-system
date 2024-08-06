ff7b1e73533869e87ba805951f127cbd0666afa5
Ademir Santos <ademir.m.junior@gmail.com>
2024-08-06 20:42:24 -0300

    implement input ports in order application service

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderAddress.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderAddress.java
@@ -15,7 +15,7 @@ public class OrderAddress {
     private final String street;
     @NotNull
     @Max(value = 10)
-    private final String postaCode;
+    private final String postalCode;
     @NotNull
     @Max(value = 50)
     private final String city;
--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
@@ -17,6 +17,10 @@ public class Restaurant extends AggregateRoot<RestaurantId> {
         active = builder.active;
     }
 
+    public static Restaurant.Builder builder() {
+        return new Restaurant.Builder();
+    }
+
     public List<Product> getProducts() {
         return products;
     }
